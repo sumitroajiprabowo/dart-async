e86ae55183c9d76faad55b1690858263fa6bbf42
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-14 23:29:27 +0700

    update broadcast_stream.dart

--- bin/broadcast_stream.dart
+++ bin/broadcast_stream.dart
@@ -14,25 +14,44 @@ Stream<int> getNumber(int number) async* { // membuat stream dari sebuah list de
 }
 
 void main(){
+
+  // example broadcast stream
+  Stream<int> simpleStream = getNumber(10);
+  Stream<int> simpleBroadcastStream = simpleStream.asBroadcastStream()
+  .take(2); // membuat broadcast stream dari sebuah list
+  print(simpleStream.isBroadcast); // cek apakah stream adalah broadcast stream (false)
+  print(simpleBroadcastStream.isBroadcast); // cek apakah stream adalah broadcast stream (true)
+  // cetak data stream
+  print('Data stream: ${simpleStream.toString()}');
+  print('Data broadcast stream: ${simpleBroadcastStream.toString()}');
+
+  /*
+  Jika kita melakukan listen terhadap simpleStream, maka akan terjadi error karena stream biasa tidak bisa memiliki banyak listener.
+  Bad state: Stream has already been listened to.
+   */
+  // simpleStream.listen((event) {print('Data: $event');}); // listen digunakan untuk menampilkan data dari sebuah stream (stream integer) dengan menggunakan stream biasa
+  // simpleStream.listen((event) {print('Data: $event');}); // ini akan terjadi error karena stream biasa tidak bisa memiliki banyak listener
+
+  // solusinya adalah dengan menggunakan broadcast stream
+  simpleBroadcastStream.listen((event) {print('Data from simpleBroadcastStream: $event');}); // listen digunakan untuk menampilkan data dari sebuah stream (stream integer) dengan menggunakan broadcast stream
+  simpleBroadcastStream.listen((event) {print('Data from simpleBroadcastStream: $event');}); // listen digunakan untuk menampilkan data dari sebuah stream (stream integer) dengan menggunakan broadcast stream
+
   // example broadcast stream
   var stream = getNumber(10).asBroadcastStream(); // membuat broadcast stream dari sebuah list
-  print(stream.isBroadcast); // cek apakah stream adalah broadcast stream (true
-  // cetak data stream
-  print('Data stream: ${stream.toString()}');
   var subscription1 = stream.listen((data) { // membuat stream subscription 1
-    print('Data 1: $data'); // cetak data
+    print('Data from subscription1: $data'); // cetak data
   }, onError: (error) { // error
-    print('Error 1: $error'); // cetak error
+    print('Error from subscription1: $error'); // cetak error
   }, onDone: () { // done
-    print('Done 1'); // cetak done
+    print('Done from subscription1'); // cetak done
   }, cancelOnError: true); // cancel on error
 
   var subscription2 = stream.listen((data) { // membuat stream subscription 2
-    print('Data 2: $data'); // cetak data
+    print('Data from subscription2: $data'); // cetak data
   }, onError: (error) { // error
-    print('Error 2: $error'); // cetak error
+    print('Error from subscription2: $error'); // cetak error
   }, onDone: () { // done
-    print('Done 2'); // cetak done
+    print('Done from subscription2'); // cetak done
   }, cancelOnError: true); // cancel on error
 
   // example broadcast stream pause
